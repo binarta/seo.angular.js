7cfc3aaf5b1c3ee46d8580976eb6675150a2cc8f
Jo Kerkhofs <jke@thinkerit.be>
2017-06-20 15:13:03 +0200

    Cleaned up seoSupport service. All tags will be added by the backend.

--- test/seo.spec.js
+++ test/seo.spec.js
@@ -22,35 +22,20 @@ describe('seo', function () {
         binarta.application.adhesiveReading.read('-');
     }
 
-    describe('on route change', function () {
-        beforeEach(function () {
-            $rootScope.$broadcast('$routeChangeStart', {params: {}});
-        });
-
-        it('canonical link is added', function () {
-            var tag = head.find('link[rel="canonical"]')[0];
-            expect(tag.href).toContain(path);
-        });
-
-        it('on path change, update canonical link', function () {
-            $location.path('/new/path');
-            $rootScope.$broadcast('$routeChangeStart', {params: {}});
-            var tag = head.find('link[rel="canonical"]')[0];
-            expect(tag.href).toContain('/new/path');
-        });
-    });
-
     describe('seoSupport service', function () {
         var siteName, defaultTitle, pageTitle, pageDescription;
 
-        describe('on resolve with default values', function () {
-            beforeEach(function () {
+        describe('on getSEOValues with default values', function () {
+            var successSpy;
+
+            beforeEach(inject(function () {
                 siteName = defaultSiteName;
                 defaultTitle = '';
                 pageTitle = '';
                 pageDescription = '';
-                $rootScope.$broadcast('$routeChangeStart', {params: {}});
-            });
+                successSpy = jasmine.createSpy('spy');
+                seoSupport.getSEOValues({success: successSpy});
+            }));
 
             it('default message are resolved', function () {
                 expect(i18n.resolveSpy['seo.site.name']).toEqual(siteName);
@@ -73,49 +58,13 @@ describe('seo', function () {
                     });
                 });
 
-                it('title tag is added', function () {
-                    var tag = head.find('title')[0];
-                    expect(tag.innerText).toEqual(defaultSiteName);
-                });
-
-                it('description tag is not added', function () {
-                    var tag = head.find('meta[name="description"]')[0];
-                    expect(tag).toBeUndefined();
-                });
-
-                it('og description meta tag is not added', function () {
-                    var tag = head.find('meta[property="og:description"]')[0];
-                    expect(tag).toBeUndefined();
-                });
-
-                it('og title meta tag is not added', function () {
-                    var tag = head.find('meta[property="og:title"]')[0];
-                    expect(tag).toBeUndefined();
-                });
-
-                it('author meta tag is added', function () {
-                    var tag = head.find('meta[name="author"]')[0];
-                    expect(tag.content).toEqual(defaultSiteName);
-                });
-
-                it('og type meta tag is added', function () {
-                    var tag = head.find('meta[property="og:type"]')[0];
-                    expect(tag.content).toEqual('website');
-                });
-
-                it('og site_name meta tag is added', function () {
-                    var tag = head.find('meta[property="og:site_name"]')[0];
-                    expect(tag.content).toEqual(defaultSiteName);
-                });
-
-                it('og url meta tag is added', function () {
-                    var tag = head.find('meta[property="og:url"]')[0];
-                    expect(tag.content).toContain(path);
+                it('success callback is executed', function () {
+                    expect(successSpy).toHaveBeenCalledWith(seoSupport.seo);
                 });
             });
         });
 
-        describe('on resolve with values', function () {
+        describe('on getSEOValues with values', function () {
             beforeEach(function () {
                 siteName = 'site name';
                 defaultTitle = 'default title';
@@ -127,7 +76,7 @@ describe('seo', function () {
                 i18n.updateSpy[path + '.seo.title'] = pageTitle;
                 i18n.updateSpy[path + '.seo.description'] = pageDescription;
 
-                $rootScope.$broadcast('$routeChangeStart', {params: {}});
+                seoSupport.getSEOValues();
             });
 
             it('message are resolved', function () {
@@ -149,46 +98,6 @@ describe('seo', function () {
                         title: pageTitle,
                         description: pageDescription
                     });
-                });
-
-                it('title tag is added', function () {
-                    var tag = head.find('title')[0];
-                    expect(tag.innerText).toEqual(pageTitle + ' | ' + siteName);
-                });
-
-                it('description tag is added', function () {
-                    var tag = head.find('meta[name="description"]')[0];
-                    expect(tag.content).toEqual(pageDescription);
-                });
-
-                it('og description meta tag is added', function () {
-                    var tag = head.find('meta[property="og:description"]')[0];
-                    expect(tag.content).toEqual(pageDescription);
-                });
-
-                it('og title meta tag is added', function () {
-                    var tag = head.find('meta[property="og:title"]')[0];
-                    expect(tag.content).toEqual(pageTitle);
-                });
-
-                it('author meta tag is added', function () {
-                    var tag = head.find('meta[name="author"]')[0];
-                    expect(tag.content).toEqual(siteName);
-                });
-
-                it('og type meta tag is added', function () {
-                    var tag = head.find('meta[property="og:type"]')[0];
-                    expect(tag.content).toEqual('website');
-                });
-
-                it('og site_name meta tag is added', function () {
-                    var tag = head.find('meta[property="og:site_name"]')[0];
-                    expect(tag.content).toEqual(siteName);
-                });
-
-                it('og url meta tag is added', function () {
-                    var tag = head.find('meta[property="og:url"]')[0];
-                    expect(tag.content).toContain(path);
                 });
             });
         });
@@ -240,110 +149,10 @@ describe('seo', function () {
                     expect(tag.innerText).toEqual(pageTitle + ' | ' + siteName);
                 });
 
-                it('description tag is updated', function () {
-                    var tag = head.find('meta[name="description"]')[0];
-                    expect(tag.content).toEqual(pageDescription);
-                });
-
-                it('og description meta tag is updated', function () {
-                    var tag = head.find('meta[property="og:description"]')[0];
-                    expect(tag.content).toEqual(pageDescription);
-                });
-
-                it('og title meta tag is updated', function () {
-                    var tag = head.find('meta[property="og:title"]')[0];
-                    expect(tag.content).toEqual(pageTitle);
-                });
-
-                it('author meta tag is updated', function () {
-                    var tag = head.find('meta[name="author"]')[0];
-                    expect(tag.content).toEqual(siteName);
-                });
-
-                it('og type meta tag is updated', function () {
-                    var tag = head.find('meta[property="og:type"]')[0];
-                    expect(tag.content).toEqual('website');
-                });
-
-                it('og site_name meta tag is updated', function () {
-                    var tag = head.find('meta[property="og:site_name"]')[0];
-                    expect(tag.content).toEqual(siteName);
-                });
-
-                it('og url meta tag is updated', function () {
-                    var tag = head.find('meta[property="og:url"]')[0];
-                    expect(tag.content).toContain(path);
-                });
-
                 it('success callback is executed', function () {
                     expect(resolved).toBeTruthy();
                 });
             });
-        });
-
-        describe('on update title tag', function () {
-            beforeEach(function () {
-                pageTitle = 'updated title';
-                seoSupport.updateTitleTag(pageTitle);
-                $rootScope.$digest();
-            });
-
-            it('title tag is updated', function () {
-                var tag = head.find('title')[0];
-                expect(tag.innerText).toEqual(pageTitle);
-            });
-
-            it('og title meta tag is updated', function () {
-                var tag = head.find('meta[property="og:title"]')[0];
-                expect(tag.content).toEqual(pageTitle);
-            });
-        });
-
-        describe('on update description tag', function () {
-            beforeEach(function () {
-                pageDescription = 'updated description';
-                seoSupport.updateDescriptionTag(pageDescription);
-                $rootScope.$digest();
-            });
-
-            it('description tag is updated', function () {
-                var tag = head.find('meta[name="description"]')[0];
-                expect(tag.content).toEqual(pageDescription);
-            });
-
-            it('og description meta tag is updated', function () {
-                var tag = head.find('meta[property="og:description"]')[0];
-                expect(tag.content).toEqual(pageDescription);
-            });
-        });
-
-        describe('on update image meta tag', function () {
-            var url;
-
-            beforeEach(function () {
-                url = '/image/path';
-                seoSupport.updateImageMetaTag(url);
-                $rootScope.$digest();
-            });
-
-            it('og image meta tag is updated', function () {
-                var tag = head.find('meta[property="og:image"]')[0];
-                expect(tag.content).toEqual(url);
-            });
-        });
-
-        it('apple icon is added to head', function () {
-            var appleIcon = head.find('link[rel="apple-touch-icon"]')[0];
-            expect(appleIcon.sizes.toString()).toEqual('180x180');
-            expect(appleIcon.href).toContain(config.awsPath + 'favicon.img?height=180');
-        });
-
-        it('favicons are added to head', function () {
-            var favicons = head.find('link[rel="icon"]');
-            expect(favicons[0].sizes.toString()).toEqual('32x32');
-            expect(favicons[0].href).toContain(config.awsPath + 'favicon.img?height=32');
-            expect(favicons[1].sizes.toString()).toEqual('16x16');
-            expect(favicons[1].href).toContain(config.awsPath + 'favicon.img?height=16');
         });
     });
 
@@ -508,160 +317,6 @@ describe('seo', function () {
                         });
                     });
                 });
-            });
-        });
-    });
-
-    describe('seoTitle directive', function () {
-        var title, element, scope;
-
-        beforeEach(function () {
-            scope = $rootScope.$new();
-            title = head.find('title')[0];
-            element = angular.element('<div seo-title>{{var}}</div>');
-        });
-
-        describe('with no previous title set', function () {
-            beforeEach(function () {
-                $compile(element)(scope);
-                scope.var = 'Page title';
-                scope.$digest();
-            });
-
-            it('title element is updated', function () {
-                expect(title.innerText).toEqual('Page title | ' + defaultSiteName);
-            });
-
-            describe('when element changes', function () {
-                beforeEach(function () {
-                    scope.var = 'changed title';
-                    scope.$digest();
-                });
-
-                it('title is updated', function () {
-                    expect(title.innerText).toEqual('changed title | ' + defaultSiteName);
-                });
-            });
-        });
-
-        describe('with a previous title', function () {
-            beforeEach(function () {
-                i18n.updateSpy[path + '.seo.title'] = 'previous title';
-                seoSupport.updateTags();
-                $compile(element)(scope);
-                scope.var = 'Page title';
-                scope.$digest();
-            });
-
-            it('title element is not updated', function () {
-                expect(title.innerText).toEqual('previous title | ' + defaultSiteName);
-            });
-
-            describe('when element changes', function () {
-                beforeEach(function () {
-                    scope.var = 'changed title';
-                    scope.$digest();
-                });
-
-                it('title is not updated', function () {
-                    expect(title.innerText).toEqual('previous title | ' + defaultSiteName);
-                });
-            });
-        });
-    });
-
-    describe('seoDescription directive', function () {
-        var description, element, scope, binTruncateSpy;
-
-        beforeEach(inject(function (_binTruncateSpy_) {
-            binTruncateSpy = _binTruncateSpy_;
-            scope = $rootScope.$new();
-            element = angular.element('<div seo-description>{{var}}</div>');
-        }));
-
-        describe('with no previous description set', function () {
-            beforeEach(function () {
-                $compile(element)(scope);
-                scope.var = 'd';
-                scope.$digest();
-                description = head.find('meta[name="description"]')[0];
-            });
-
-            it('description tag is updated', function () {
-                expect(binTruncateSpy).toHaveBeenCalledWith({
-                    value: 'd',
-                    length: 160
-                });
-                expect(description.content).toEqual('d');
-            });
-
-            describe('when element changes', function () {
-                beforeEach(function () {
-                    scope.var = 'changed description';
-                    scope.$digest();
-                });
-
-                it('description tag is updated', function () {
-                    expect(binTruncateSpy).toHaveBeenCalledWith({
-                        value: 'changed description',
-                        length: 160
-                    });
-                    expect(description.content).toEqual('changed description');
-                });
-            });
-        });
-
-        describe('with previous description', function () {
-            beforeEach(function () {
-                i18n.updateSpy[path + '.seo.description'] = 'previous description';
-                seoSupport.updateTags();
-                $compile(element)(scope);
-                scope.var = 'd';
-                scope.$digest();
-                description = head.find('meta[name="description"]')[0];
-            });
-
-            it('description tag is not updated', function () {
-                expect(description.content).toEqual('previous description');
-            });
-
-            describe('when element changes', function () {
-                beforeEach(function () {
-                    scope.var = 'changed description';
-                    scope.$digest();
-                });
-
-                it('description tag is not updated', function () {
-                    expect(description.content).toEqual('previous description');
-                });
-            });
-        });
-    });
-
-    describe('seoImage directive', function () {
-        var tag, element, scope;
-
-        beforeEach(function () {
-            scope = $rootScope.$new();
-            element = angular.element('<image src="{{var}}" seo-image />');
-            $compile(element)(scope);
-            scope.var = 'http://image-url.jpg/';
-            scope.$digest();
-            tag = head.find('meta[property="og:image"]')[0];
-        });
-
-        it('image meta tag is added', function () {
-            expect(tag.content).toEqual('http://image-url.jpg/');
-        });
-
-        describe('when element changes', function () {
-            beforeEach(function () {
-                scope.var = 'http://another-url.jpg/';
-                scope.$digest();
-            });
-
-            it('image meta tag is updated', function () {
-                expect(tag.content).toEqual('http://another-url.jpg/');
             });
         });
     });
