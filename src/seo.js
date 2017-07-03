8ea4a9c334175517ae2b5dcaf83c5ad0f2a28fc3
Jo Kerkhofs <jke@thinkerit.be>
2017-07-03 13:21:37 +0200

    Revert "Cleaned up seoSupport service. All tags will be added by the backend."
    This reverts commit 7cfc3aaf5b1c3ee46d8580976eb6675150a2cc8f.

--- src/seo.js
+++ src/seo.js
@@ -1,10 +1,18 @@
 (function () {
     angular.module('seo', ['binarta-applicationjs-angular1', 'binarta-checkpointjs-angular1', 'i18n', 'config',
         'toggle.edit.mode', 'ngRoute', 'angularx'])
-        .service('seoSupport', ['$q', '$document', 'i18n', 'config', 'binarta', SeoSupportService])
-        .directive('seoSupport', ['editModeRenderer', 'seoSupport', 'binarta', seoSupportDirectiveFactory]);
+        .service('seoSupport', ['$location', '$q', '$document', 'i18n', 'config', 'binarta', SeoSupportService])
+        .directive('seoSupport', ['editModeRenderer', 'seoSupport', 'binarta', seoSupportDirectiveFactory])
+        .directive('seoTitle', ['seoSupport', SeoTitleDirective])
+        .directive('seoDescription', ['$filter', 'seoSupport', SeoDescriptionDirective])
+        .directive('seoImage', ['seoSupport', SeoImageDirective])
+        .run(['seoSupport', '$rootScope', function (seoSupport, $rootScope) {
+            $rootScope.$on('$routeChangeStart', function () {
+                seoSupport.updateTags();
+            });
+        }]);
 
-    function SeoSupportService($q, $document, i18n, config, binarta) {
+    function SeoSupportService($location, $q, $document, i18n, config, binarta) {
         var self = this;
         var head = $document.find('head');
 
@@ -43,29 +51,79 @@
                     title: args.title,
                     description: args.description
                 };
-                updateTitleTag();
+                updateTags();
                 if (args && args.success) args.success();
             });
+        };
+
+        this.updateTags = function () {
+            this.getSEOValues({
+                success: updateTags
+            });
+            updateCanonicalLinkTag();
         };
 
         function getNamespace() {
             return config.namespace.charAt(0).toUpperCase() + config.namespace.substring(1);
         }
 
-        function updateTitleTag() {
-            var title = self.seo.title || self.seo.defaultTitle || '';
+        function updateTags() {
+            self.updateTitleTag();
+            self.updateDescriptionTag();
+            updateMetaTags();
+        }
+
+        this.updateTitleTag = function (t) {
+            var title = self.seo.title || t || self.seo.defaultTitle || '';
             var siteName = self.seo.siteName || '';
             var divider = title && siteName ? ' | ' : '';
             var pageTitle = title + divider + siteName;
             updateTag({tag: 'title', content: pageTitle});
-        }
+            updateMetaTag({property: 'og:title', content: title});
+        };
+
+        this.updateDescriptionTag = function (d) {
+            var description = self.seo.description || d || '';
+            updateMetaTag({name: 'description', content: description});
+            updateMetaTag({property: 'og:description', content: description});
+        };
+
+        this.updateImageMetaTag = function (url) {
+            updateMetaTag({property: 'og:image', content: url});
+        };
 
         function updateTag(args) {
             var result = head.find(args.tag);
-            if (result.length === 0) {
+            if (result.length == 0) {
                 if (args.content) head.prepend('<' + args.tag + '>' + args.content + '</' + args.tag + '>');
             } else args.content ? result[0].textContent = args.content : result[0].remove();
         }
+
+        function updateMetaTags() {
+            updateMetaTag({name: 'author', content: self.seo.siteName});
+            updateMetaTag({property: 'og:type', content: 'website'});
+            updateMetaTag({property: 'og:site_name', content: self.seo.siteName});
+            updateMetaTag({property: 'og:url', content: $location.absUrl()});
+        }
+
+        function updateMetaTag(args) {
+            var type = args.property ? 'property' : 'name';
+            var id = args.property || args.name;
+            var result = head.find('meta[' + type + '="' + id + '"]');
+            if (result.length == 0) {
+                if (args.content) head.append('<meta ' + type + '="' + id + '" content="' + args.content + '">');
+            } else args.content ? result[0].content = args.content : result[0].remove();
+        }
+
+        function updateCanonicalLinkTag() {
+            var result = head.find('link[rel="canonical"]');
+            var href = $location.absUrl();
+            result.length == 0 ? head.append('<link rel="canonical" href="' + href + '">') : result[0].href = href;
+        }
+
+        head.append('<link rel="apple-touch-icon" sizes="180x180" href="' + config.awsPath + 'favicon.img?height=180">');
+        head.append('<link rel="icon" type="image/png" sizes="32x32" href="' + config.awsPath + 'favicon.img?height=32">');
+        head.append('<link rel="icon" type="image/png" sizes="16x16" href="' + config.awsPath + 'favicon.img?height=16">');
     }
 
     function seoSupportDirectiveFactory(editModeRenderer, seoSupport, binarta) {
@@ -129,6 +187,65 @@
                         scope: editScope
                     });
                 };
+            }
+        }
+    }
+
+    function SeoTitleDirective(seoSupport) {
+        return {
+            restrict: 'A',
+            link: function (scope, el) {
+                seoSupport.getSEOValues({
+                    success: onSuccess
+                });
+
+                function onSuccess(seo) {
+                    if (!seo.title) watchOnElement();
+                }
+
+                function watchOnElement() {
+                    scope.$watch(function () {
+                        return el[0].innerText;
+                    }, function (value) {
+                        if (value) seoSupport.updateTitleTag(value);
+                    });
+                }
+            }
+        }
+    }
+
+    function SeoDescriptionDirective($filter, seoSupport) {
+        return {
+            restrict: 'A',
+            link: function (scope, el) {
+                seoSupport.getSEOValues({
+                    success: onSuccess
+                });
+
+                function onSuccess(seo) {
+                    if (!seo.description) watchOnElement();
+                }
+
+                function watchOnElement() {
+                    scope.$watch(function () {
+                        return el[0].innerText;
+                    }, function (value) {
+                        if (value) seoSupport.updateDescriptionTag($filter('binTruncate')(value, 160));
+                    });
+                }
+            }
+        }
+    }
+
+    function SeoImageDirective(seoSupport) {
+        return {
+            restrict: 'A',
+            link: function (scope, el) {
+                scope.$watch(function () {
+                    return el[0].src;
+                }, function (value) {
+                    if (value) seoSupport.updateImageMetaTag(value);
+                });
             }
         }
     }
